59bb880a2025d79e4227ca440550c75bb54450b8
brittany duncan <duncandakota08@gmail.com>
2023-11-13 03:46:22 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(anyNumber) {
+  if (anyNumber <= 400) {
+    return "This one is on me!";
+  } else if (anyNumber > 400 && anyNumber <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (anyNumber > 2000 && anyNumber <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
+}
+function ternaryCheckCity(city){
+    if (city === "NYC"){
+        return "Ok, sounds good.";
+    }   else{
+          return "No go.";
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+    if (tip === "generous"){
+      return "Thank you so much.";
+    } else if (tip === "not as generous"){
+      return "Thank you."
+    } else {
+      return "Bye."
+    }
 }
